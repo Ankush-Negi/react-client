bd2c77cf466401d1926a079288ea73f785e677c5
Ankush-Negi <ankush.negi@successive.tech>
2020-05-29 15:41:58 +0530

    feat: feature branch created

--- src/pages/Trainee/TraineeList.jsx
+++ src/pages/Trainee/TraineeList.jsx
@@ -1,8 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
-import { AddDialog, TableComponent } from './components/index';
+import propTypes from 'prop-types';
+import * as moment from 'moment';
+import { AddDialog, TableComponent } from './components';
 import trainee from './data/trainee';
 
 const useStyles = {
@@ -17,63 +18,84 @@ class TraineeList extends React.Component {
     super(props);
     this.state = {
       open: false,
+      order: 'asc',
+      orderBy: 'Date',
     };
   }
 
-   handleClickOpen = () => {
-     this.setState({ open: true });
-   };
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
 
-   handleClose = () => {
-     this.setState({ open: false });
-   };
+  handleClose = () => {
+    this.setState({ open: false });
+  };
 
-   onSubmitHandle = (values) => {
-     this.setState({ open: false });
-     console.log(values);
-   }
+  handleSubmit = (values) => {
+    this.setState({ open: false });
+    console.log(values);
+  }
 
-   render() {
-     const { open } = this.state;
-     const { match: { url }, classes } = this.props;
-     return (
-       <>
-         <div className={classes.button}>
-           <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
-       Add Trainee
-           </Button>
-         </div>
-         <AddDialog open={open} onClose={this.handleClose} onSubmit={this.onSubmitHandle} />
-         <TableComponent
+  handleSort = (value) => {
+    const { orderBy, order } = this.state;
+    const isAsc = orderBy === value && order === 'asc';
+    const data = isAsc ? 'desc' : 'asc';
+    this.setState({
+      order: data,
+      orderBy: value,
+    });
+  }
 
-           id="id"
+  handleSelectChange = (value) => {
+    console.log(value);
+  }
 
-           data={trainee}
 
-           column={[{
-             field: 'name',
-             label: 'Name',
-             align: 'center',
-           },
-           {
-             field: 'email',
-             label: 'Email-Address',
+  render() {
+    const { open, order, orderBy } = this.state;
+    const { classes } = this.props;
+    const getDateFormatted = (date) => moment(date).format('dddd,MMMM Do YYYY, h:mm:ss a');
+    return (
+      <>
+        <div className={classes.button}>
+          <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+            Add Trainee
+          </Button>
+        </div>
+        <AddDialog open={open} onClose={this.handleClose} onSubmit={this.handleSubmit} />
+        <TableComponent
+          id="id"
+          data={trainee}
+          column={[{
+            field: 'name',
+            label: 'Name',
+          },
+          {
+            field: 'email',
+            label: 'Email-Address',
+            format: (value) => value && value.toUpperCase(),
 
-           }]}
+          },
+          {
+            field: 'createdAt',
+            label: 'Date',
+            align: 'right',
+            format: getDateFormatted,
+          }]}
 
-         />
-         <ul>
-           {trainee.length && trainee.map((data) => (
-             <Fragment key={data.id}>
-               <li>
-                 <Link to={`${url}/${data.id}`}>{data.name}</Link>
-               </li>
+          order={order}
+          orderBy={orderBy}
+          onSort={this.handleSort}
+          onSelect={this.handleSelectChange}
 
-             </Fragment>
-           ))}
-         </ul>
-       </>
-     );
-   }
+        />
+      </>
+    );
+  }
 }
 export default withStyles(useStyles, { withTheme: true })(TraineeList);
+TraineeList.propTypes = {
+  // used in previous task
+  // match: propTypes.objectOf(propTypes.any).isRequired,
+  classes: propTypes.objectOf(propTypes.any).isRequired,
+};
--- src/pages/Trainee/components/Table/Table.jsx
+++ src/pages/Trainee/components/Table/Table.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import propTypes from 'prop-types';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -23,12 +42,33 @@ const useStyles = makeStyles((theme) => ({
   color: {
     color: 'grey',
   },
+  visuallyHidden: {
+    border: 0,
+    clip: 'rect(0 0 0 0)',
+    height: 1,
+    margin: -1,
+    overflow: 'hidden',
+    padding: 0,
+    position: 'absolute',
+    top: 20,
+    width: 1,
+  },
+
 }));
 
 const TableComponent = (props) => {
   const classes = useStyles();
-
-  const { id, data, column } = props;
+  const [open, setOpen] = React.useState(false);
+  const {
+    id, data, column, order, orderBy, onSort, onSelect,
+  } = props;
+  const handleSortIcon = (e) => {
+    e.target.style.color = 'black';
+    setOpen(true);
+  };
+  const handleColorChange = (e) => {
+    e.target.style.color = 'grey';
+  };
 
   return (
     <TableContainer component={Paper} className={classes.container}>
@@ -36,20 +76,44 @@ const TableComponent = (props) => {
         <TableHead>
           <TableRow key={id}>
             {column.length && column.map((col) => (
-              <TableCell align={col.align} className={classes.color}>{col.label}</TableCell>
+              <TableCell
+                align={col.align}
+                className={classes.color}
+              >
+                <TableSortLabel
+                  onMouseEnter={handleSortIcon}
+                  onMouseLeave={handleColorChange}
+                  onBlur={handleColorChange}
+                  active={orderBy === col.field}
+                  direction={orderBy === col.field ? order : 'asc'}
+                  onClick={() => onSort(col.field)}
+                  hideSortIcon={open}
+                >
+                  <>
+                    {col.label}
+                    {orderBy === col.field ? (
+                      <span className={classes.visuallyHidden}>
+                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                      </span>
+                    ) : null}
+                  </>
+                </TableSortLabel>
+              </TableCell>
             ))}
 
           </TableRow>
         </TableHead>
         <TableBody>
           {data.length && data.map((element) => (
-            <TableRow key={element[id]}>
-              {column.map(({ field, align }) => (
+            <StyledTableRow hover onClick={() => onSelect(element)} key={element.id}>
+              {column.map(({ field, align, format }) => (
 
-                <TableCell align={align}>{element[field]}</TableCell>
+                <StyledTableCell align={align}>
+                  {format ? format(element[field]) : element[field]}
+                </StyledTableCell>
 
               ))}
-            </TableRow>
+            </StyledTableRow>
 
           ))}
 
@@ -65,4 +129,12 @@ TableComponent.propTypes = {
   id: propTypes.string.isRequired,
   data: propTypes.arrayOf(propTypes.object).isRequired,
   column: propTypes.arrayOf(propTypes.object).isRequired,
+  order: propTypes.oneOf(['asc', 'desc']),
+  orderBy: propTypes.string,
+  onSort: propTypes.func.isRequired,
+  onSelect: propTypes.func.isRequired,
+};
+TableComponent.defaultProps = {
+  order: 'asc',
+  orderBy: '',
 };
